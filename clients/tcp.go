b91c717012349e950e22dedc18337ddcbe37fb12
JT Archie <jtarchie@gmail.com>
2021-02-05 20:19:28 -0700

    benchmark TCP server

--- clients/tcp.go
+++ clients/tcp.go
@@ -2,8 +2,11 @@ package clients
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"net"
 	"net/url"
+	"time"
 )
 
 type TCPClient struct {
@@ -13,9 +16,19 @@ type TCPClient struct {
 var _ Client = &TCPClient{}
 
 func NewTCP(uri *url.URL) (*TCPClient, error) {
-	connection, err := net.Dial("tcp", fmt.Sprintf("%s:%s", uri.Hostname(), uri.Port()))
+	var connection net.Conn
+	err := retry(func() error {
+		var err error
+
+		connection, err = net.Dial("tcp", fmt.Sprintf("%s:%s", uri.Hostname(), uri.Port()))
+		if err != nil {
+			return fmt.Errorf("could not create TCP client (%s:%s): %w", uri.Hostname(), uri.Port(), err)
+		}
+
+		return nil
+	})
 	if err != nil {
-		return nil, fmt.Errorf("could not create TCP client (%s:%s): %w", uri.Hostname(), uri.Port(), err)
+		return nil, fmt.Errorf("not successful after retries: %w", err)
 	}
 
 	return &TCPClient{
@@ -40,6 +53,30 @@ func (t *TCPClient) Close() error {
 	err := t.connection.Close()
 	if err != nil {
 		return fmt.Errorf("could not close TCP client (%s): %w", t.connection.LocalAddr(), err)
+	}
+
+	return nil
+}
+
+func retry(fn func() error) error {
+	retries := 3
+
+	for i := 1; true; i++ {
+		err := fn()
+		if err != nil {
+			if i < retries {
+				duration := time.Duration(math.Exp2(float64(i))) * time.Millisecond
+
+				//nolint:gosec
+				jitter := time.Duration(rand.Intn(10)) * time.Millisecond
+
+				time.Sleep(duration + jitter)
+				continue
+			}
+
+			return err
+		}
+		break
 	}
 
 	return nil
---
+++ listeners/tcp_benchmark_test.go
@@ -0,0 +1,63 @@
+package listeners_test
+
+import (
+	"fmt"
+	"github.com/jtarchie/jsyslog/clients"
+	"github.com/jtarchie/jsyslog/listeners"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"sync/atomic"
+	"time"
+)
+
+var _ = Describe("UDP server", func() {
+	Measure("handles many messages", func(b Benchmarker) {
+		stopClientServer := make(chan struct{})
+
+		port, err := listeners.NextReusablePort()
+		Expect(err).NotTo(HaveOccurred())
+
+		server, err := listeners.New(fmt.Sprintf("tcp://0.0.0.0:%d", port))
+		Expect(err).NotTo(HaveOccurred())
+
+		var receivedCounter int32
+		go func() {
+			err = server.ListenAndServe(func(_ string) error {
+				select {
+				case <-stopClientServer:
+					return fmt.Errorf("time finished")
+				default:
+					atomic.AddInt32(&receivedCounter, 1)
+					return nil
+				}
+			})
+		}()
+
+		client, err := clients.New(fmt.Sprintf("tcp://0.0.0.0:%d", port))
+		Expect(err).NotTo(HaveOccurred())
+
+		b.Time("sending messages", func() {
+			timer := time.NewTimer(1 * time.Second)
+
+			var sentCounter int32
+			go func() {
+				for {
+					select {
+					case <-stopClientServer:
+						return
+					default:
+						_ = client.WriteString(listeners.PlaceholderValid5424)
+						atomic.AddInt32(&sentCounter, 1)
+					}
+				}
+			}()
+
+			<-timer.C
+			close(stopClientServer)
+
+			b.RecordValue("number of messages sent", float64(atomic.LoadInt32(&sentCounter)))
+			b.RecordValue("number of messages processes", float64(atomic.LoadInt32(&receivedCounter)))
+		})
+
+	}, 5)
+})
